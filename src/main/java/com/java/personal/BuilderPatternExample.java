ceaa17ef52a59a09d68879936cef2a5e2cf7edbd
Gustavo4 <gustavordt@outlook.com>
2025-06-30 20:22:59 +0200

    adding old builder pattern

--- src/main/java/com/java/personal/BuilderPatternExample.java
+++ src/main/java/com/java/personal/BuilderPatternExample.java
@@ -2,7 +2,7 @@ package com.java.personal;
 
 public class BuilderPatternExample {
     //Builder patter using Java 17 records feature
-    public record User(String name, String email) {
+    /*public record User(String name, String email) {
         public static Builder builder() {
             return new Builder();
         }
@@ -26,9 +26,59 @@ public class BuilderPatternExample {
             }
 
         }
+    }*/
+
+    public static class User {
+        private final String name;
+        private final String email;
+
+        private User(Builder builder) {
+            this.name = builder.getName();
+            this.email = builder.getEmail();
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    '}';
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static class Builder {
+            private String name;
+            private String email;
+
+            public String getName() {
+                return name;
+            }
+
+            public String getEmail() {
+                return email;
+            }
+
+            public Builder name(String name) {
+                this.name = name;
+                return this;
+            }
+
+            public Builder email(String email) {
+                this.email = email;
+                return this;
+            }
+
+            public User build() {
+                return new User(this);
+            }
+        }
     }
 
     public static void main(String[] args) {
+        // Builder pattern in java
         var user = User.builder().name("user 1").email("user@test.com").build();
         System.out.printf("User 1 information: %s%n",user.toString());
     }
